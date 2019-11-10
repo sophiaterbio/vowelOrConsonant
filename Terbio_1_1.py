2879ead4370e4514a407438fb69bf81e3f744fa7
sophiaterbio <47809709+sophiaterbio@users.noreply.github.com>
2019-11-11 01:18:19 +0800

    Add files via upload

---
+++ Terbio_1_1.py
@@ -0,0 +1,7 @@
+let = (input('Enter a letter to see if it is a vowel or a consonant: '))
+
+if (let == 'A' or let == 'a' or let == 'E' or let == 'e' or let == 'I' or let == 'i' 
+or let == 'O' or let == 'o' or let == 'U' or let == 'u'): 
+    print ('The character "',let,'" is a vowel.')
+else:
+    print ('The character "',let,'" is a consonant.')
